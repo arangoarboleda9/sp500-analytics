9bb9623528dfdd5f25c01abc7eacf85b75e16580
carlosmdq44 <carlos_mdq44@hotmail.com>
2025-11-21 20:20:47 -0300

    Apply terraform fmt to network.tf

--- infra/terraform/network.tf
+++ infra/terraform/network.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
+  cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
@@ -12,7 +12,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "public_1a" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block = var.public_subnet_1a_cidr
+  cidr_block              = var.public_subnet_1a_cidr
   availability_zone       = "${var.aws_region}a"
   map_public_ip_on_launch = true
 

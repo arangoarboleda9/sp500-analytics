92654f68d5de08395c568433644086927fff6d60
carlosmdq44 <carlos_mdq44@hotmail.com>
2025-12-08 14:51:52 -0300

    chore(terraform): remover config duplicada de auto-stop

--- infra/terraform/auto_stop_airflow.tf
+++
@@ -1,5 +0,0 @@
-data "archive_file" "stop_airflow_zip" {
-  type        = "zip"
-  source_file = "${path.module}/../lambda/stop_airflow_if_idle.py"
-  output_path = "${path.module}/../lambda/stop_airflow_if_idle.zip"
-}
